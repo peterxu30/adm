7346c137f47b45fdc291f32845d218697b9b093c
Peter Xu <peterxu30@users.noreply.github.com>
2016-12-18 19:43:55 -0800

    wrote some timeseries data log tests

--- logger_test.go
+++ logger_test.go
@@ -147,7 +147,10 @@ func TestInsertSimpleUuidMetadata(t *testing.T) {
 
 	for i := 0; i < 100; i++ {
 		uuid := strconv.Itoa(i)
-		log.updateUuidMetadataStatus(uuid, NOT_STARTED)
+		err := log.updateUuidMetadataStatus(uuid, NOT_STARTED)
+		if err != nil {
+			t.Fatal("inserting metadata failed", uuid)
+		}
 	}
 
 	for i := 0; i < 100; i++ {
@@ -160,7 +163,10 @@ func TestInsertSimpleUuidMetadata(t *testing.T) {
 
 	for i := 0; i < 100; i++ {
 		uuid := strconv.Itoa(i)
-		log.updateUuidMetadataStatus(uuid, WRITE_START)
+		err := log.updateUuidMetadataStatus(uuid, WRITE_START)
+		if err != nil {
+			t.Fatal("inserting metadata failed", uuid)
+		}
 	}
 
 	for i := 0; i < 100; i++ {
@@ -173,7 +179,10 @@ func TestInsertSimpleUuidMetadata(t *testing.T) {
 
 	for i := 0; i < 100; i++ {
 		uuid := strconv.Itoa(i)
-		log.updateUuidMetadataStatus(uuid, WRITE_COMPLETE)
+		err := log.updateUuidMetadataStatus(uuid, WRITE_COMPLETE)
+		if err != nil {
+			t.Fatal("inserting metadata failed", uuid)
+		}
 	}
 
 	for i := 0; i < 100; i++ {
@@ -194,12 +203,16 @@ func TestInsertStripedUuidMetadata(t *testing.T) {
 
 	for i := 0; i < 1000; i ++ {
 		uuid := strconv.Itoa(i)
+		var err error
 		if i % 3 == 0 {
-			log.updateUuidMetadataStatus(uuid, NOT_STARTED)
+			err = log.updateUuidMetadataStatus(uuid, NOT_STARTED)
 		} else if i % 3 == 1 {
-			log.updateUuidMetadataStatus(uuid, WRITE_START)
+			err = log.updateUuidMetadataStatus(uuid, WRITE_START)
 		} else {
-			log.updateUuidMetadataStatus(uuid, WRITE_COMPLETE)
+			err = log.updateUuidMetadataStatus(uuid, WRITE_COMPLETE)
+		}
+		if err != nil {
+			t.Fatal("inserting metadata failed", uuid)
 		}
 	}
 
@@ -225,9 +238,210 @@ func TestInsertStripedUuidMetadata(t *testing.T) {
 	testTeardown()
 }
 
-func TestInsertUuidTimeseriesdata(t *testing.T) {}
+func TestUpdateUuidMetadata(t *testing.T) {
+	testStartup()
 
-func TestUpdateUuidMetadata(t *testing.T) {}
+	log := newTestLog()
+
+	for i := 0; i < 1000; i++ {
+		uuid := strconv.Itoa(i)
+		err := log.updateUuidMetadataStatus(uuid, NOT_STARTED)
+		if err != nil {
+			t.Fatal("inserting metadata failed", uuid)
+		}
+	}
+
+	for i := 0; i < 1000; i++ {
+		uuid := strconv.Itoa(i)
+		if log.getUuidMetadataStatus(uuid) != NOT_STARTED {
+			t.Fatal(uuid, "should have status NOT_STARTED")
+		}
+	}
+
+	for i := 0; i < 1000; i++ {
+		uuid := strconv.Itoa(i)
+		err := log.updateUuidMetadataStatus(uuid, WRITE_START)
+		if err != nil {
+			t.Fatal("inserting metadata failed", uuid)
+		}
+	}
+
+	for i := 0; i < 1000; i++ {
+		uuid := strconv.Itoa(i)
+		if log.getUuidMetadataStatus(uuid) != WRITE_START {
+			t.Fatal(uuid, "should have status WRITE_START")
+		}
+	}
+
+	for i := 0; i < 1000; i++ {
+		uuid := strconv.Itoa(i)
+		err := log.updateUuidMetadataStatus(uuid, WRITE_COMPLETE)
+		if err != nil {
+			t.Fatal("inserting metadata failed", uuid)
+		}
+	}
+
+	for i := 0; i < 1000; i++ {
+		uuid := strconv.Itoa(i)
+		if log.getUuidMetadataStatus(uuid) != WRITE_COMPLETE {
+			t.Fatal(uuid, "should have status WRITE_START")
+		}
+	}
+
+	testTeardown()
+}
+
+func TestInsertSimpleUuidTimeseriesData(t *testing.T) {
+	testStartup()
+
+	log := newTestLog()
+
+	for i := 0; i < 30; i++ {
+		uuid := strconv.Itoa(i)
+		for time := 0; time < 3; time++ {
+			slot := &TimeSlot {
+				Uuid: uuid,
+				StartTime: int64(time),
+				EndTime: int64(time + 1),
+			}
+			err := log.updateUuidTimeseriesStatus(slot, NOT_STARTED)
+			if err != nil {
+				t.Fatal("inserting timeseries data failed", uuid)
+			}
+		}	
+	}
+
+	for i := 0; i < 30; i++ {
+		uuid := strconv.Itoa(i)
+		for time := 0; time < 3; time++ {
+			slot := &TimeSlot {
+				Uuid: uuid,
+				StartTime: int64(time),
+				EndTime: int64(time + 1),
+			}
+			if log.getUuidTimeseriesStatus(slot) != NOT_STARTED {
+				t.Fatal(uuid, "should have status NOT_STARTED")
+			}
+		}
+	}
+
+	for i := 0; i < 30; i++ {
+		uuid := strconv.Itoa(i)
+		for time := 0; time < 3; time++ {
+			slot := &TimeSlot {
+				Uuid: uuid,
+				StartTime: int64(time),
+				EndTime: int64(time + 1),
+			}
+			err := log.updateUuidTimeseriesStatus(slot, WRITE_START)
+			if err != nil {
+				t.Fatal("inserting timeseries data failed", uuid)
+			}
+		}	
+	}
+
+	for i := 0; i < 30; i++ {
+		uuid := strconv.Itoa(i)
+		for time := 0; time < 3; time++ {
+			slot := &TimeSlot {
+				Uuid: uuid,
+				StartTime: int64(time),
+				EndTime: int64(time + 1),
+			}
+			if log.getUuidTimeseriesStatus(slot) != WRITE_START {
+				t.Fatal(uuid, "should have status WRITE_START")
+			}
+		}
+	}
+
+	for i := 0; i < 30; i++ {
+		uuid := strconv.Itoa(i)
+		for time := 0; time < 3; time++ {
+			slot := &TimeSlot {
+				Uuid: uuid,
+				StartTime: int64(time),
+				EndTime: int64(time + 1),
+			}
+			err := log.updateUuidTimeseriesStatus(slot, WRITE_COMPLETE)
+			if err != nil {
+				t.Fatal("inserting timeseries data failed", uuid)
+			}
+		}	
+	}
+
+	for i := 0; i < 30; i++ {
+		uuid := strconv.Itoa(i)
+		for time := 0; time < 3; time++ {
+			slot := &TimeSlot {
+				Uuid: uuid,
+				StartTime: int64(time),
+				EndTime: int64(time + 1),
+			}
+			if log.getUuidTimeseriesStatus(slot) != WRITE_COMPLETE {
+				t.Fatal(uuid, "should have status WRITE_COMPLETE")
+			}
+		}
+	}
+
+	testTeardown()
+}
+
+func TestInsertStripedUuidTimeseriesData(t *testing.T) {
+	testStartup()
+
+	log := newTestLog()
+
+	for i := 0; i < 30; i++ {
+		uuid := strconv.Itoa(i)
+		for time := 0; time < 3; time++ {
+			slot := &TimeSlot {
+				Uuid: uuid,
+				StartTime: int64(time),
+				EndTime: int64(time + 1),
+			}
+			mod := (i + time) % 3
+			var err error
+			if mod == 0 {
+				err = log.updateUuidTimeseriesStatus(slot, NOT_STARTED)			
+			} else if mod == 1 {
+				err = log.updateUuidTimeseriesStatus(slot, WRITE_START)
+			} else {
+				err = log.updateUuidTimeseriesStatus(slot, WRITE_COMPLETE)
+			}
+			if err != nil {
+				t.Fatal("inserting timeseries data failed", uuid)
+			}
+		}
+	}
+
+	for i := 0; i < 30; i++ {
+		uuid := strconv.Itoa(i)
+		for time := 0; time < 3; time++ {
+			slot := &TimeSlot {
+				Uuid: uuid,
+				StartTime: int64(time),
+				EndTime: int64(time + 1),
+			}
+
+			mod := (i + time) % 3
+			if mod == 0 {
+				if log.getUuidTimeseriesStatus(slot) != NOT_STARTED {
+					t.Fatal(uuid, "should have status NOT_STARTED")
+				}			
+			} else if mod == 1 {
+				if log.getUuidTimeseriesStatus(slot) != WRITE_START {
+					t.Fatal(uuid, "should have status WRITE_START")
+				}
+			} else {
+				if log.getUuidTimeseriesStatus(slot) != WRITE_COMPLETE {
+					t.Fatal(uuid, "should have status WRITE_COMPLETE")
+				}
+			}
+		}
+	}
+
+	testTeardown()
+}
 
 func TestUpdateUuidTimeseriesdata(t *testing.T) {}
 
